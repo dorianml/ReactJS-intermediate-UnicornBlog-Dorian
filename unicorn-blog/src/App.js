637c55b8bfe56c20c3ccd9727d3558f1a36c7ebd
dmjahed <mjahedlucas.dorian@gmail.com>
2022-06-21 10:38:19 +0200

    Add First Draft

---
+++ unicorn-blog/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='Logo'>
+          LOGO
+        </div>
+        <div className='Nav-links'>
+          <a>
+            Upload
+          </a>
+          <a>
+            Galery
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
